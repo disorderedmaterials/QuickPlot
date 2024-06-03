80e6f82281c69aac44f09c6c528ef7197a6347b6
Adam Washington <adam.washington@stfc.ac.uk>
2024-06-03 13:34:49 +0100

    feat: Add scatter plot support (#17)
    * feat: Add scatter plot support
    
    * Fix cmake formatting

---
+++ src/ScatterModel.qml
@@ -0,0 +1,32 @@
+import QtQuick
+import QtQuick3D
+import com.projectdissolve
+
+Model {
+    id: root
+
+    property color color
+    property double thickness
+    property Axis xAxis
+    property list<double> xs
+    property Axis yAxis
+    property list<double> ys
+
+    geometry: ScatterGeometry {
+        id: plotLine
+
+        thickness: root.thickness
+        xAxis: root.xAxis
+        xs: root.xs
+        yAxis: root.yAxis
+        ys: root.ys
+    }
+    materials: [
+        PrincipledMaterial {
+            id: frame_material
+
+            alphaMode: PrincipledMaterial.Opaque
+            baseColor: root.color
+        }
+    ]
+}
---
+++ src/scatterGeometry.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "axis.h"
+#include <QQuick3DGeometry>
+#include <QVector3D>
+
+class ScatterGeometry : public QQuick3DGeometry
+{
+    Q_OBJECT
+    QML_NAMED_ELEMENT(ScatterGeometry)
+    Q_PROPERTY(double thickness MEMBER thickness_ NOTIFY dataChanged)
+    Q_PROPERTY(QList<double> xs MEMBER xs_ NOTIFY dataChanged)
+    Q_PROPERTY(QList<double> ys MEMBER ys_ NOTIFY dataChanged)
+    Q_PROPERTY(Axis *xAxis MEMBER xAxis_ NOTIFY dataChanged)
+    Q_PROPERTY(Axis *yAxis MEMBER yAxis_ NOTIFY dataChanged)
+
+    public:
+    ScatterGeometry();
+
+    Q_SIGNALS:
+    void dataChanged();
+
+    private:
+    void updateData();
+    double thickness_ = 0.01;
+    QList<double> xs_, ys_;
+    Axis *xAxis_, *yAxis_;
+};
