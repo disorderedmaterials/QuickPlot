10c0fa42f081b1e4d53fc046875379dd9c22802b
Adam Washington <adam.washington@stfc.ac.uk>
2024-06-03 10:24:53 +0100

    Use Dissolve official clang-format

--- src/axisTickLabels.h
+++ src/axisTickLabels.h
@@ -4,20 +4,20 @@
 
 class Axis;
 
-class AxisTickLabels : public QAbstractListModel {
-  Q_OBJECT
+class AxisTickLabels : public QAbstractListModel
+{
+    Q_OBJECT
 
-public:
-  AxisTickLabels(Axis &parent);
+    public:
+    AxisTickLabels(Axis &parent);
 
-  int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-  QVariant data(const QModelIndex &index,
-                int role = Qt::DisplayRole) const override;
-  QHash<int, QByteArray> roleNames() const override;
-  int tickCount() const;
-  void setTickCount(const int count);
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+    int tickCount() const;
+    void setTickCount(const int count);
 
-private:
-  Axis &parent_;
-  int N_;
+    private:
+    Axis &parent_;
+    int N_;
 };
