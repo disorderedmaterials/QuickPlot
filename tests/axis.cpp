71434deb29b193a32e0e4b2631cd3881e33b4876
Adam Washington <adam.washington@stfc.ac.uk>
2024-06-18 15:10:46 +0100

    Test tick labels

--- tests/axis.cpp
+++ tests/axis.cpp
@@ -1,8 +1,41 @@
 #include "axis.h"
+#include "logAxis.h"
 #include <gtest/gtest.h>
 
 namespace UnitTest
 {
 
-TEST(AxisTest, TickMarks) { EXPECT_EQ(1, 2); }
+void matchAxes(Axis &axis, std::vector<double> &points)
+{
+    axis.dataChanged();
+    ASSERT_EQ(axis.tickCount(), points.size());
+
+    for (int i = 0; i < axis.tickCount(); i++)
+    {
+        EXPECT_DOUBLE_EQ(points[i], axis.tick(i));
+    }
+}
+
+TEST(AxisTest, LinearMarks)
+{
+    Axis axis;
+    axis.setMinimum(-11.0);
+    axis.setMaximum(11.0);
+
+    std::vector<double> points = {-10, -5, 0, 5, 10};
+
+    matchAxes(axis, points);
+}
+
+TEST(AxisTest, LogMarks)
+{
+    LogAxis axis;
+    axis.setMinimum(0.1);
+    axis.setMaximum(10.0);
+
+    std::vector<double> points = {0.1, 0.2, 0.3, 0.5, 0.8, 1.0, 2.0, 3.0, 5.0, 8.0, 10.0};
+
+    matchAxes(axis, points);
+}
+
 } // namespace UnitTest
