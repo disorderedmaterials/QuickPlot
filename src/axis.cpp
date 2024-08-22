610c82b96d565413e4afd015cfe8ff22a98626dd
Adam Washington <adam.washington@stfc.ac.uk>
2024-08-22 13:31:38 +0100

    Start giving thicker axes
    They're not right, but at least it's not longer a single face

--- src/axis.cpp
+++ src/axis.cpp
@@ -7,6 +7,15 @@
 #include <cmath>
 #include <iostream>
 
+struct Vec3
+{
+    float x, y, z;
+};
+
+const Vec3 xhat = Vec3{1, 0, 0};
+const Vec3 yhat = Vec3{0, 1, 0};
+const Vec3 zhat = Vec3{0, 0, 1};
+
 Axis::Axis() : minimum_(-1), maximum_(1), direction_(Axis::Direction::Y), thickness_(0.001), tickLabels_(*this)
 {
     updateData();
@@ -50,6 +59,49 @@ void Axis::updateTicks_()
     }
 }
 
+void draw_tube(float *p, float thickness, Vec3 v1, Vec3 v2, Vec3 n1, Vec3 n2)
+{
+    bool a = false;
+    bool b = false;
+    for (auto i = 0; i < 4; ++i)
+    {
+
+        *p++ = v1.x - ((a ? n1.x : -n1.x) + (b ? n2.x : -n2.x)) * thickness;
+        *p++ = v1.y - ((a ? n1.y : -n1.y) + (b ? n2.y : -n2.y)) * thickness;
+        *p++ = v1.z - ((a ? n1.z : -n1.z) + (b ? n2.z : -n2.z)) * thickness;
+
+        *p++ = v2.x - ((a ? n1.x : -n1.x) + (b ? n2.x : -n2.x)) * thickness;
+        *p++ = v2.y - ((a ? n1.y : -n1.y) + (b ? n2.y : -n2.y)) * thickness;
+        *p++ = v2.z - ((a ? n1.z : -n1.z) + (b ? n2.z : -n2.z)) * thickness;
+
+        *p++ = v2.x + ((a ? n1.x : -n1.x) + (b ? n2.x : -n2.x)) * thickness;
+        *p++ = v2.y + ((a ? n1.y : -n1.y) + (b ? n2.y : -n2.y)) * thickness;
+        *p++ = v2.z + ((a ? n1.z : -n1.z) + (b ? n2.z : -n2.z)) * thickness;
+
+        *p++ = v2.x + ((a ? n1.x : -n1.x) + (b ? n2.x : -n2.x)) * thickness;
+        *p++ = v2.y + ((a ? n1.y : -n1.y) + (b ? n2.y : -n2.y)) * thickness;
+        *p++ = v2.z + ((a ? n1.z : -n1.z) + (b ? n2.z : -n2.z)) * thickness;
+
+        *p++ = v1.x + ((a ? n1.x : -n1.x) + (b ? n2.x : -n2.x)) * thickness;
+        *p++ = v1.y + ((a ? n1.y : -n1.y) + (b ? n2.y : -n2.y)) * thickness;
+        *p++ = v1.z + ((a ? n1.z : -n1.z) + (b ? n2.z : -n2.z)) * thickness;
+
+        *p++ = v1.x - ((a ? n1.x : -n1.x) + (b ? n2.x : -n2.x)) * thickness;
+        *p++ = v1.y - ((a ? n1.y : -n1.y) + (b ? n2.y : -n2.y)) * thickness;
+        *p++ = v1.z - ((a ? n1.z : -n1.z) + (b ? n2.z : -n2.z)) * thickness;
+
+        if (a)
+        {
+            a = false;
+            b = !b;
+        }
+        else
+        {
+            a = true;
+        }
+    }
+}
+
 void Axis::updateData()
 {
     clear();
@@ -59,77 +111,19 @@ void Axis::updateData()
 
     int stride = 3 * sizeof(float);
 
-    QByteArray vertexData(6 * stride, Qt::Initialization::Uninitialized);
+    QByteArray vertexData(4 * 6 * stride, Qt::Initialization::Uninitialized);
     float *p = reinterpret_cast<float *>(vertexData.data());
 
     switch (direction_)
     {
         case Axis::Direction::X:
-            *p++ = -1.0;
-            *p++ = -1.0 - thickness_;
-            *p++ = 0;
-            *p++ = 1.0;
-            *p++ = -1.0 - thickness_;
-            *p++ = 0;
-            *p++ = 1.0;
-            *p++ = -1.0 + thickness_;
-            *p++ = 0;
-
-            *p++ = 1.0;
-            *p++ = -1.0 + thickness_;
-            *p++ = 0;
-            *p++ = -1.0;
-            *p++ = -1.0 + thickness_;
-            *p++ = 0;
-            *p++ = -1.0;
-            *p++ = -1.0 - thickness_;
-            *p++ = 0;
+            draw_tube(p, thickness_, Vec3{-1.0, -1.0, 0}, Vec3{1.0, -1.0, 0}, yhat, zhat);
             break;
         case Axis::Direction::Y:
-            *p++ = -1.0 - thickness_;
-            *p++ = -1.0;
-            *p++ = 0;
-            *p++ = -1.0 + thickness_;
-            *p++ = 1.0;
-            *p++ = 0;
-            *p++ = -1.0 - thickness_;
-            *p++ = 1.0;
-            *p++ = 0;
-
-            *p++ = -1.0 - thickness_;
-            *p++ = -1.0;
-            *p++ = 0;
-            *p++ = -1.0 + thickness_;
-            *p++ = -1.0;
-            *p++ = 0;
-            *p++ = -1.0 + thickness_;
-            *p++ = 1.0;
-            *p++ = 0;
+            draw_tube(p, thickness_, Vec3{-1.0, -1.0, 0}, Vec3{-1.0, 1.0, 0}, zhat, xhat);
             break;
         case Axis::Direction::Z:
-            *p++ = -1.0;
-            *p++ = -1.0 - thickness_;
-            *p++ = 0;
-
-            *p++ = -1.0;
-            *p++ = -1.0 + thickness_;
-            *p++ = 4.0;
-
-            *p++ = -1.0;
-            *p++ = -1.0 - thickness_;
-            *p++ = 4.0;
-
-            *p++ = -1.0;
-            *p++ = -1.0 - thickness_;
-            *p++ = 0;
-
-            *p++ = -1.0;
-            *p++ = -1.0 + thickness_;
-            *p++ = 0;
-
-            *p++ = -1.0;
-            *p++ = -1.0 + thickness_;
-            *p++ = 4.0;
+            draw_tube(p, thickness_, Vec3{-1.0, -1.0, 0}, Vec3{-1.0, -1.0, 4}, xhat, yhat);
             break;
     }
 
