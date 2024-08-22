f72c087ce450ace510793e7612c8b26ba26b66bf
Adam Washington <adam.washington@stfc.ac.uk>
2024-08-22 17:36:30 +0100

    Simplify drawing with quads

--- src/solid.cpp
+++ src/solid.cpp
@@ -1,60 +1,29 @@
 #include "solid.h"
+#include "triangle.h"
 
 float *draw_tube(float *p, float thickness, Vec3<float> v1, Vec3<float> v2, Vec3<float> n1, Vec3<float> n2)
 {
-    // -n1
-    p = (v1 - (n1 + n2) * thickness).write(p);
-    p = (v1 - (n1 - n2) * thickness).write(p);
-    p = (v2 - (n1 - n2) * thickness).write(p);
 
-    p = (v2 - (n1 - n2) * thickness).write(p);
-    p = (v2 - (n1 + n2) * thickness).write(p);
-    p = (v1 - (n1 + n2) * thickness).write(p);
+    Quad q;
+    q = Quad(v1 - (n1 + n2) * thickness, v1 - (n1 - n2) * thickness, v2 - (n1 - n2) * thickness, v2 - (n1 + n2) * thickness);
+    p = q.writeByteArray(p);
 
-    // +n1
-    p = (v1 + (n1 + n2) * thickness).write(p);
-    p = (v1 + (n1 - n2) * thickness).write(p);
-    p = (v2 + (n1 - n2) * thickness).write(p);
+    q = Quad(v1 + (n1 - n2) * thickness, v1 + (n1 + n2) * thickness, v2 + (n1 + n2) * thickness, v2 + (n1 - n2) * thickness);
+    p = q.flip().writeByteArray(p);
 
-    p = (v2 + (n1 - n2) * thickness).write(p);
-    p = (v2 + (n1 + n2) * thickness).write(p);
-    p = (v1 + (n1 + n2) * thickness).write(p);
+    q = Quad(v1 - (n2 + n1) * thickness, v1 - (n2 - n1) * thickness, v2 - (n2 - n1) * thickness, v2 - (n2 + n1) * thickness);
+    p = q.flip().writeByteArray(p);
 
-    // +n2
-    p = (v1 - (n1 - n2) * thickness).write(p);
-    p = (v1 + (n1 + n2) * thickness).write(p);
-    p = (v2 - (n1 - n2) * thickness).write(p);
+    q = Quad(v1 + (n2 - n1) * thickness, v1 + (n2 + n1) * thickness, v2 + (n2 + n1) * thickness, v2 + (n2 - n1) * thickness);
+    p = q.writeByteArray(p);
 
-    p = (v2 + (n1 + n2) * thickness).write(p);
-    p = (v2 - (n1 - n2) * thickness).write(p);
-    p = (v1 + (n1 + n2) * thickness).write(p);
+    // end caps
 
-    // // -n2
-    p = (v1 + (n1 - n2) * thickness).write(p);
-    p = (v1 - (n1 + n2) * thickness).write(p);
-    p = (v2 - (n1 + n2) * thickness).write(p);
+    q = Quad(v1 + (n1 - n2) * thickness, v1 + (n1 + n2) * thickness, v1 + (n2 - n1) * thickness, v1 - (n1 + n2) * thickness);
+    p = q.writeByteArray(p);
 
-    p = (v2 - (n1 + n2) * thickness).write(p);
-    p = (v2 + (n1 - n2) * thickness).write(p);
-    p = (v1 + (n1 - n2) * thickness).write(p);
-
-    // v1 cap
-    p = (v1 - (n1 + n2) * thickness).write(p);
-    p = (v1 + (n1 - n2) * thickness).write(p);
-    p = (v1 - (n1 - n2) * thickness).write(p);
-
-    p = (v1 + (n1 + n2) * thickness).write(p);
-    p = (v1 - (n1 - n2) * thickness).write(p);
-    p = (v1 + (n1 - n2) * thickness).write(p);
-
-    // v2 cap
-    p = (v2 + (n1 - n2) * thickness).write(p);
-    p = (v2 - (n1 + n2) * thickness).write(p);
-    p = (v2 - (n1 - n2) * thickness).write(p);
-
-    p = (v2 - (n1 - n2) * thickness).write(p);
-    p = (v2 + (n1 + n2) * thickness).write(p);
-    p = (v2 + (n1 - n2) * thickness).write(p);
+    q = Quad(v2 + (n1 - n2) * thickness, v2 + (n1 + n2) * thickness, v2 + (n2 - n1) * thickness, v2 - (n1 + n2) * thickness);
+    p = q.flip().writeByteArray(p);
 
     return p;
 }
